4d05fda1f85c9f583f9e03eefa857a6b077b0050
Devanshu <devanshukansal11@gmail.com>
2026-03-04 01:07:25 +0530

    graph series

---
+++ Concepts/1-matrixGraphRepresentation.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>adj(n+1,vector<int>(n+1,0));
+    
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        
+        adj[u][v]=1;
+        adj[v][u]=1;
+    }
+    
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=n;j++){
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+// TC- O(n^2)
+// SC- O(n^2);
+
+// undirected graph
+// 1-2 
+// 1-3
+// 3-4
+// 2-4
+// 2-5 
+// 4-5
+// No of nodes-5 and no of edges-6
